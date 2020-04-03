40bc6c8b83b7a8dc76e9aaabb01246ff739ec133
uriel melendez <uriel.melendez@outlook.es>
2020-04-03 15:27:04 -0600

    Methods as lambdas examples

---
+++ src/lambdas_01_03/Lambdas_01_03.java
@@ -0,0 +1,44 @@
+package lambdas_01_03;
+
+import java.math.BigInteger;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.IntFunction;
+import java.util.function.UnaryOperator;
+
+public class Lambdas_01_03 {
+
+	public static void main(String[] args) {
+		
+		IntFunction<String> intToString = num -> Integer.toString(num);
+		
+		System.out.println("expected value 3, actual value :"
+				  + intToString.apply(123).length());
+		
+		//static method reference using ::
+		IntFunction<String> intToString2 = Integer::toString;
+		System.out.println("expected value 4, actual value :"
+				  + intToString.apply(4567).length());
+		
+		//Lambdas made using constructor
+		Function<String,BigInteger> newBigInteger = BigInteger::new;
+		System.out.println("expected value 123456789, actual value :"
+				  + newBigInteger.apply("123456789"));
+		
+		//example of a lambda made from an instance method
+		Consumer<String> print = System.out::println;
+		print.accept("Coming to you directly from a lambda...");
+		
+		
+		//these two are the same using the static method concat
+		UnaryOperator<String> greeting = x -> "hello, ".concat(x);
+		System.out.println("world");
+		
+		UnaryOperator<String> makeGreeting = "hello, "::concat;
+		System.out.print(makeGreeting.apply("Peggy"));
+		
+		
+		
+	}
+
+}
